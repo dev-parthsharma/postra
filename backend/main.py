273166813920f8ef7573583b8d42e0f72d15d43d
Parth Sharma <sharmaaparth.07@gmail.com>
2026-05-03 12:44:57 +0530

    fix: allow cors

--- backend/main.py
+++ backend/main.py
@@ -8,14 +8,7 @@ app = FastAPI(title="Postra API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://postraa.vercel.app"
-        "https://localhost:5173",
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=[*],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
